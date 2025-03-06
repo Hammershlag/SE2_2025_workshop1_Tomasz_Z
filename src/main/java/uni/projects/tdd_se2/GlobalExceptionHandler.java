85c894ef8d1d288b5a3808b78d592588f22f6614
tomek <tomekzbroszczyk@gmail.com>
2025-03-06 10:11:46 +0100

    Initial commit

---
+++ src/main/java/uni/projects/tdd_se2/GlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package uni.projects.tdd_se2;
+
+/**
+     * @author  Tomasz Zbroszczyk
+     * @since   27.02.2025
+     * @version 1.0
+     */public class GlobalExceptionHandler {
+}
---
+++ src/test/java/uni/projects/tdd_se2/TddSe2ApplicationTests.java
@@ -0,0 +1,13 @@
+package uni.projects.tdd_se2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TddSe2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
